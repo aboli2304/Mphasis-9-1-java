7b91d8e1537f62d7dffb63a2f74fb10f0dac0180
github_aboli2304 <abolicholachgudd1223@gmail.com>
2022-02-09 16:22:10 +0530

    Class Object and Pillers

---
+++ FirstProject/src/main/java/com/simplilearn/demo/ServletOne.java
@@ -0,0 +1,19 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServlet;
+
+
+public class ServletOne extends HttpServlet {
+	
+      @Override
+      protected void doGet(Https Req)
+      
+      PrintWrite out =resp.getWriter();
+      
+      out.print("Welcome to my First Web Application");
+}
